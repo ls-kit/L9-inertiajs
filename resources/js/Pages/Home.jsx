ddd6303956493c9d11237f22a9c281d4cf8ff0b2
git/rayhanrobbani <rayhan@lskit.com>
2023-02-06 13:51:34 +0600

    initial-commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Guest from '../Layouts/Guest'
+
+export default function Home() {
+  return (
+    <>
+        <div className='container'>
+          <div className="card">
+            <div className="card-body">
+              Your home page
+              test page
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+Home.layout = (page) => <Guest children={page} title={"Home"}/>
